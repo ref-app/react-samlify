261f95c367544d5bb66a0bb01303e9ee139f17e7
Per Bergland <per@refapp.se>
2019-08-20 19:18:52 +0200

    Add jwt types

--- services/auth.ts
+++ services/auth.ts
@@ -1,30 +1,30 @@
 /**
  * Service Layer
  */
-import * as jwt from 'jsonwebtoken';
+import * as jwt from "jsonwebtoken";
 
-const SECRET = 'somethingverysecret';
+const SECRET = "somethingverysecret";
 
 // this is a mock function, it should be used to interact with your database in real use case
 export function getUser(login: string) {
-  if (login === 'user.passify.io@gmail.com') {
+  if (login === "user.passify.io@gmail.com") {
     return {
-      user_id: '21b06b08-f296-42f4-81aa-73fb5a8eac67',
+      user_id: "21b06b08-f296-42f4-81aa-73fb5a8eac67",
       email: login
     };
   }
   return null;
 }
 
-export function createToken(payload) {
+export function createToken(payload: string | object | Buffer) {
   return jwt.sign(payload, SECRET);
 }
 
-export function verifyToken(token) {
+export function verifyToken(token: string) {
   try {
     const payload = jwt.verify(token, SECRET);
     return { verified: true, payload: payload };
-  } catch(err) {
+  } catch (err) {
     return { verified: false, payload: null };
   }
 }
